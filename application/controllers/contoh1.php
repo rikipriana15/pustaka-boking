e5a491d876745133448fa763b8961fd38539d251
WP2_RIKI ANDRIAN PRIANA <90885477+rikipriana15@users.noreply.github.com>
2021-12-19 17:38:29 +0700

    contoh1.php

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -4,9 +4,9 @@ class Contoh1 extends CI_Controller
  public function index()
  {
  echo "<h1>Perkenalkan</h1>";
- echo"Nama saya Hasna Aulia Hendriastuti dengan NIM : 12200400 Kelas : 12.3A.13.
- Saya tinggal di daerah Semplak.
+ echo"Nama saya Riki andrian priana dengan NIM : 12200017 Kelas : 12.3A.13.
+ Saya tinggal di daerah leuwiliang.
  Hal yang saya sukai adalah
- Bernyanyi, berolahraga, dan berwisata kuliner";
+ hiking dan berolahraga";
  }
 }
